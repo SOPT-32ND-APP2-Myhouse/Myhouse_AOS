3905018376b69840765bfc857595c4ee58761775
Sangwook123 <operawook@catholic.ac.kr>
2023-05-22 06:39:12 +0900

    [feat] 유저들의 비슷한 공간 베스트 리사이클러뷰

---
+++ app/src/main/java/com/example/myhouse_aos/presentation/content/adapter/ItemContentUserBestAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.myhouse_aos.presentation.content.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myhouse_aos.databinding.ItemContentUserbestBinding
+import com.example.myhouse_aos.domain.model.UserBestData
+import com.example.myhouse_aos.util.extension.ItemDiffCallback
+
+class ItemContentUserBestAdapter : ListAdapter<UserBestData, ItemContentUserBestAdapter.ItemContentUserBestViewHolder>(
+    ItemDiffCallback<UserBestData>(
+        onItemsTheSame = {old, new -> old.image == new.image},
+        onContentsTheSame = {old, new -> old == new}
+    )
+) {
+    class ItemContentUserBestViewHolder(private val binding: ItemContentUserbestBinding) :
+        RecyclerView.ViewHolder(binding.root){
+        fun onBind(data: UserBestData){
+            binding.data = data
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemContentUserBestAdapter.ItemContentUserBestViewHolder {
+        val binding = ItemContentUserbestBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ItemContentUserBestAdapter.ItemContentUserBestViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemContentUserBestViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+}
