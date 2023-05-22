320c6c8c979917935ec9630a3967e11407a3dfed
HyeseonBaek <bhs12087@naver.com>
2023-05-22 23:50:43 +0900

    [feat] viewmodel에 dummy data 추가, 어댑터 연결해 구현

--- app/src/main/java/com/example/myhouse_aos/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/myhouse_aos/presentation/home/HomeFragment.kt
@@ -1,20 +1,92 @@
 package com.example.myhouse_aos.presentation.home
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.myhouse_aos.R
 import com.example.myhouse_aos.databinding.FragmentHomeBinding
 import com.example.myhouse_aos.util.binding.BindingFragment
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
-    private lateinit var viewModel: HomeViewModel
-
+    private val viewModel by viewModels<HomeViewModel>()
+    private lateinit var popularContentsAdapter: PopularContentsAdapter
+    private lateinit var recommendHomeAdapter: RecommendHomeAdapter
+    private lateinit var recommendProductAdapter: RecommendProductAdapter
+    private lateinit var modernContentsAdapter: ModernContentsAdapter
+    private lateinit var categoryAdapter: CategoryAdapter
+    private lateinit var summerContentsAdapter: SummerContentsAdapter
+    private lateinit var colorInteriorAdapter: ColorInteriorAdapter
+    private lateinit var colorProductAdapter: ColorProductAdapter
+    private lateinit var reviewAdapter: ReviewAdapter
+    private lateinit var menuAdapter: MenuAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        getPopularContents()
+        getRecommendHome()
+        getRecommendProduct()
+        getModernInterior()
+        getCategory()
+        getSummerContents()
+        getColorInterior()
+        getColorProduct()
+        getReview()
+        getMenu()
+    }
+
+    private fun initAdapter(){
+        popularContentsAdapter = PopularContentsAdapter()
+        recommendHomeAdapter = RecommendHomeAdapter()
+        recommendProductAdapter = RecommendProductAdapter()
+        modernContentsAdapter = ModernContentsAdapter()
+        categoryAdapter = CategoryAdapter()
+        summerContentsAdapter = SummerContentsAdapter()
+        colorInteriorAdapter = ColorInteriorAdapter()
+        colorProductAdapter = ColorProductAdapter()
+        reviewAdapter = ReviewAdapter()
+        menuAdapter = MenuAdapter()
+
+        binding.rvHomePopularContents.adapter = popularContentsAdapter
+        binding.rvHomeRecommendHouse.adapter = recommendHomeAdapter
+        binding.rvHomeRecommendProduct.adapter = recommendProductAdapter
+        binding.rvHomeModernInterior.adapter = modernContentsAdapter
+        binding.rvHomeFindCategoryProduct.adapter = categoryAdapter
+        binding.rvHomeSummerContent.adapter = summerContentsAdapter
+        binding.rvHomeColorInterior.adapter = colorInteriorAdapter
+        binding.rvHomeColorProduct.adapter = colorProductAdapter
+        binding.rvHomeReview.adapter = reviewAdapter
+        binding.rvHomeMenu.adapter = menuAdapter
+    }
+
+    private fun getPopularContents() {
+        popularContentsAdapter.submitList(viewModel.popularContentsList)
+    }
+    private fun getRecommendHome() {
+        recommendHomeAdapter.submitList(viewModel.recommendHomeList)
+    }
+    private fun getRecommendProduct() {
+        recommendProductAdapter.submitList(viewModel.recommendProductList)
+    }
+    private fun getModernInterior() {
+        modernContentsAdapter.submitList(viewModel.modernInteriorList)
+    }
+    private fun getCategory() {
+        categoryAdapter.submitList(viewModel.categoryList)
+    }
+    private fun getSummerContents() {
+        summerContentsAdapter.submitList(viewModel.summerContentsList)
+    }
+    private fun getColorInterior() {
+        colorInteriorAdapter.submitList(viewModel.colorInteriorList)
+    }
+    private fun getColorProduct() {
+        colorProductAdapter.submitList(viewModel.colorProductList)
+    }
+    private fun getReview() {
+        reviewAdapter.submitList(viewModel.reviewList)
+    }
+    private fun getMenu() {
+        menuAdapter.submitList(viewModel.menuList)
     }
 }
--- app/src/main/java/com/example/myhouse_aos/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/myhouse_aos/presentation/home/HomeViewModel.kt
@@ -1,7 +1,260 @@
 package com.example.myhouse_aos.presentation.home
 
 import androidx.lifecycle.ViewModel
+import com.example.myhouse_aos.R
+import com.example.myhouse_aos.domain.model.ContentsData
 
 class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val popularContentsList = listOf(
+        ContentsData(
+            contentId = 1,
+            image = R.drawable.img_popular_contents,
+            description = "간결해진 삶에 행복을 더하\n는! 뷰맛집 상큼하우스",
+        ),
+        ContentsData(
+            contentId = 2,
+            image = R.drawable.img_popular_contents,
+            description = "간결해진 삶에 행복을 더하\n는! 뷰맛집 상큼하우스",
+        ),
+        ContentsData(
+            contentId = 3,
+            image = R.drawable.img_popular_contents,
+            description = "간결해진 삶에 행복을 더하\n는! 뷰맛집 상큼하우스",
+        ),
+        ContentsData(
+            contentId = 4,
+            image = R.drawable.img_popular_contents,
+            description = "간결해진 삶에 행복을 더하\n는! 뷰맛집 상큼하우스",
+        ),
+    )
+
+    val recommendHomeList = listOf(
+        ContentsData(
+            contentId = 1,
+            image = R.drawable.img_recommend_home,
+            description = "깔끔하게 변신한 20살 아파트",
+        ),
+        ContentsData(
+            contentId = 2,
+            image = R.drawable.img_recommend_home,
+            description = "소형평수&반려묘를 위한 아이디\n어! 24평 구축 리모델링",
+        ),
+        ContentsData(
+            contentId = 3,
+            image = R.drawable.img_recommend_home,
+            description = "조명맛집! 자취 4년 차의 서울 \n신축 6평 원룸",
+        ),
+        ContentsData(
+            contentId = 4,
+            image = R.drawable.img_recommend_home,
+            description = "로망집 ! 흙을 밟으며 마음껏 뛰노\n는 마당 있는 집",
+        ),
+    )
+
+    val recommendProductList = listOf(
+        ContentsData(
+            contentId = 1,
+            image = R.drawable.img_recommend_product,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 2,
+            image = R.drawable.img_recommend_product,
+            description = ""
+
+        ),
+        ContentsData(
+            contentId = 3,
+            image = R.drawable.img_recommend_product,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 4,
+            image = R.drawable.img_recommend_product,
+            description = ""
+        ),
+    )
+
+
+    val modernInteriorList = listOf(
+        ContentsData(
+            contentId = 1,
+            image = R.drawable.img_modern_interior,
+            description = "간결해진 삶에 행복을 더하는! 뷰\n맛집, 상큼 하우스"
+        ),
+        ContentsData(
+            contentId = 2,
+            image = R.drawable.img_modern_interior,
+            description = "간결해진 삶에 행복을 더하는! 뷰\n맛집, 상큼 하우스"
+
+        ),
+        ContentsData(
+            contentId = 3,
+            image = R.drawable.img_modern_interior,
+            description = "간결해진 삶에 행복을 더하는! 뷰\n맛집, 상큼 하우스"
+        ),
+        ContentsData(
+            contentId = 4,
+            image = R.drawable.img_modern_interior,
+            description = "간결해진 삶에 행복을 더하는! 뷰\n맛집, 상큼 하우스"
+        ),
+    )
+
+    val categoryList = listOf(
+        ContentsData(
+            contentId = 1,
+            image = R.drawable.ic_lamp,
+            description = "조명"
+        ),
+        ContentsData(
+            contentId = 2,
+            image = R.drawable.ic_plant,
+            description = "데코 식물"
+
+        ),
+        ContentsData(
+            contentId = 3,
+            image = R.drawable.ic_furniture,
+            description = "가구"
+        ),
+        ContentsData(
+            contentId = 4,
+            image = R.drawable.ic_kitchen,
+            description = "주방용품"
+        ),
+        ContentsData(
+            contentId = 5,
+            image = R.drawable.ic_lamp,
+            description = "조명"
+        ),
+    )
+
+    val summerContentsList = listOf(
+        ContentsData(
+            contentId = 1,
+            image = R.drawable.img_summer_contents,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 2,
+            image = R.drawable.img_summer_contents,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 3,
+            image = R.drawable.img_summer_contents,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 4,
+            image = R.drawable.img_summer_contents,
+            description = ""
+        ),
+    )
+
+    val colorInteriorList = listOf(
+        ContentsData(
+            contentId = 1,
+            image = R.drawable.img_color_interior,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 2,
+            image = R.drawable.img_color_interior,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 3,
+            image = R.drawable.img_color_interior,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 4,
+            image = R.drawable.img_color_interior,
+            description = ""
+        ),
+    )
+    val colorProductList = listOf(
+        ContentsData(
+            contentId = 1,
+            image = R.drawable.img_color_product,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 2,
+            image = R.drawable.img_color_product,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 3,
+            image = R.drawable.img_color_product,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 4,
+            image = R.drawable.img_color_product,
+            description = ""
+        ),
+    )
+    val reviewList = listOf(
+        ContentsData(
+            contentId = 1,
+            image = R.drawable.img_review,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 2,
+            image = R.drawable.img_review,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 3,
+            image = R.drawable.img_review,
+            description = ""
+        ),
+        ContentsData(
+            contentId = 4,
+            image = R.drawable.img_review,
+            description = ""
+        ),
+    )
+    val menuList = listOf(
+        ContentsData(
+            contentId = 1,
+            image = R.drawable.ic_cart,
+            description = "쇼핑하기"
+        ),
+        ContentsData(
+            contentId = 2,
+            image = R.drawable.ic_sale,
+            description = "오!세일"
+        ),
+        ContentsData(
+            contentId = 3,
+            image = R.drawable.ic_discount,
+            description = "오늘의 딜"
+        ),
+        ContentsData(
+            contentId = 4,
+            image = R.drawable.ic_heart,
+            description = "취향의 발견"
+        ),
+        ContentsData(
+            contentId = 5,
+            image = R.drawable.ic_food,
+            description = "장보기"
+        ),
+        ContentsData(
+            contentId = 6,
+            image = R.drawable.ic_home,
+            description = "집들이"
+        ),
+        ContentsData(
+            contentId = 7,
+            image = R.drawable.ic_cart,
+            description = "쇼핑하기"
+        ),
+
+    )
+
 }
