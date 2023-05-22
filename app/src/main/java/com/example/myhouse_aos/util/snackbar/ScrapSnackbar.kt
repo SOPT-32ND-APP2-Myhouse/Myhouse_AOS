3267848fddf35cc41cfb3662bc17f0d164668bdc
Sangwook123 <operawook@catholic.ac.kr>
2023-05-22 13:12:03 +0900

    [feat] 스낵바 커스텀

---
+++ app/src/main/java/com/example/myhouse_aos/util/snackbar/ScrapSnackbar.kt
@@ -0,0 +1,68 @@
+package com.example.myhouse_aos.util.snackbar
+
+import android.annotation.SuppressLint
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.example.myhouse_aos.R
+import com.example.myhouse_aos.databinding.CustomSnackbarBinding
+import com.google.android.material.snackbar.Snackbar
+
+class ScrapSnackBar(view: View, private val message: String) {
+
+    companion object {
+
+        fun make(view: View, message: String) = ScrapSnackBar(view, message)
+    }
+
+    private val context = view.context
+    private val snackbar = Snackbar.make(view, "", 5000)
+    private val snackbarLayout = snackbar.view as Snackbar.SnackbarLayout
+
+    private val inflater = LayoutInflater.from(context)
+    private val snackbarBinding: CustomSnackbarBinding = DataBindingUtil.inflate(inflater, R.layout.custom_snackbar, null, false)
+    private val scrapButton = snackbarBinding.btnCustomSnackbarScrap
+    private val folderButton = snackbarBinding.btnCustomSnackbarFolder
+
+    init {
+        initView()
+        initData()
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun initView() {
+        with(snackbarLayout) {
+            removeAllViews()
+            setPadding(0, 0, 0, 0)
+            setBackgroundColor(ContextCompat.getColor(context, android.R.color.white))
+            addView(snackbarBinding.root, 0)
+
+            val params = layoutParams as ViewGroup.MarginLayoutParams
+            params.width = ViewGroup.LayoutParams.MATCH_PARENT
+            params.height = ViewGroup.LayoutParams.WRAP_CONTENT
+            params.setMargins(0, 0, 0, 0)
+            layoutParams = params
+            clipToPadding = false
+            clipChildren = false
+
+            scrapButton.setOnClickListener{
+
+            }
+            folderButton.setOnClickListener{
+
+            }
+        }
+    }
+
+    private fun initData() {
+    }
+
+    fun show() {
+        snackbar.animationMode = Snackbar.ANIMATION_MODE_SLIDE
+        snackbar.show()
+    }
+}
