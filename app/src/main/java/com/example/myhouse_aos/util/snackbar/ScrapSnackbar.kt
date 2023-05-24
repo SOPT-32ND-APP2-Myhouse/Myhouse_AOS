8c444108ffd72b3bd4c5ddbeaa1c597f1ade7b5e
Dan2dani <eldms0406@naver.com>
2023-05-24 23:38:07 +0900

    [mod] 스크랩 스낵바 수정

--- app/src/main/java/com/example/myhouse_aos/util/snackbar/ScrapSnackbar.kt
+++ app/src/main/java/com/example/myhouse_aos/util/snackbar/ScrapSnackbar.kt
@@ -1,30 +1,26 @@
 package com.example.myhouse_aos.util.snackbar
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.myhouse_aos.R
 import com.example.myhouse_aos.databinding.CustomSnackbarBinding
+import com.example.myhouse_aos.presentation.scrap.ScrapActivity
 import com.google.android.material.snackbar.Snackbar
 
 
-class ScrapSnackBar(view: View) {
-
-    companion object {
-        fun make(view: View) = ScrapSnackBar(view)
-    }
-
+class ScrapSnackBar(
+    view: View,
+) {
     private val context = view.context
     private val snackbar = Snackbar.make(view, "", 3000)
     private val snackbarLayout = snackbar.view as Snackbar.SnackbarLayout
 
     private val inflater = LayoutInflater.from(context)
-    private val snackbarBinding: CustomSnackbarBinding = DataBindingUtil.inflate(inflater, R.layout.custom_snackbar, null, false)
-    private val scrapButton = snackbarBinding.btnCustomSnackbarScrap
-    private val folderButton = snackbarBinding.btnCustomSnackbarFolder
+    private val snackbarBinding: CustomSnackbarBinding =
+        DataBindingUtil.inflate(inflater, R.layout.custom_snackbar, null, false)
 
     init {
         initView()
@@ -37,28 +33,17 @@ class ScrapSnackBar(view: View) {
             setBackgroundColor(ContextCompat.getColor(context, android.R.color.white))
             addView(snackbarBinding.root, 0)
 
-            val params = layoutParams as ViewGroup.MarginLayoutParams
-            params.width = ViewGroup.LayoutParams.MATCH_PARENT
-            params.height = ViewGroup.LayoutParams.WRAP_CONTENT
-            params.setMargins(0, 0, 0, 0)
-            layoutParams = params
-            clipToPadding = false
-            clipChildren = false
-
-            scrapButton.setOnClickListener{
-
+            snackbarBinding.btnCustomSnackbarScrap.setOnClickListener {
+                context.startActivity(Intent(context, ScrapActivity::class.java))
             }
-            folderButton.setOnClickListener{
+            snackbarBinding.btnCustomSnackbarFolder.setOnClickListener {
 
             }
         }
     }
+
     fun show() {
         snackbar.animationMode = Snackbar.ANIMATION_MODE_SLIDE
         snackbar.show()
-    }
-
-    fun dismiss(){
-        snackbar.dismiss()
     }
 }
