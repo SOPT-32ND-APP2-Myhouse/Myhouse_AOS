e1f016655b465050cc0af7bbabb802bc576635a5
Dan2dani <eldms0406@naver.com>
2023-05-25 12:45:50 +0900

    [feat] 폴더, 스크랩 어뎁터 구현

--- app/src/main/java/com/example/myhouse_aos/domain/model/Folder.kt
+++ app/src/main/java/com/example/myhouse_aos/domain/model/Folder.kt
@@ -1,13 +1,7 @@
 package com.example.myhouse_aos.domain.model
 
-
 data class Folder(
     val folderId: Int,
     val folderTitle: String,
     val scraps: List<Scrap>,
-)
-
-data class Scrap(
-    val imageUrl: String,
-    val scrapId: Int,
 )
